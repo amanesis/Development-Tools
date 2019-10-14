eddcfdc4e69d04d3cd77d1f835246c8456ea7d24
amanesis <amanesis@protonmail.com>
2019-10-14 21:49:30 +0300

    a,b fixed

--- Project 1/Part1/regr.sh
+++ Project 1/Part1/regr.sh
@@ -67,11 +67,11 @@ do                                                                              
     SUMY=$(echo "scale=2; ${SUMY} + ${Y_ARRAY[$((i + FIXER))]}" | bc)
     SUMXY=$(echo "scale=2; ${SUMXY} + ${X_ARRAY[$((i + FIXER))]} * ${Y_ARRAY[$((i + FIXER))]}" | bc)
 
-    B=$(echo "scale=2; (${LINES_IN_FILES[$COUNTER3]} * ${SUMXY} - ${SUMX} * ${SUMY}) / (${LINES_IN_FILES[$COUNTER3]} * ${SUMX2} - ${SUMX} * ${SUMX})" | bc)
-    A=$(echo "scale=2; (${SUMY} - ${B} * ${SUMX}) / ${LINES_IN_FILES[$COUNTER3]}" | bc)
-    ERR1=$(echo "scale=2; ${SUMY}/1" | bc)
-    ERR2=$(echo "scale=2; (${A} * ${SUMX} + ${B})/1" | bc )
-    ERR=$(echo "scale=2; (${ERR1} - ${ERR2}) * (${ERR1} - ${ERR2})" | bc )      #In manual pages of bc explained that subtraction won't read scale variable so we divide by 1
+    A=$(echo "scale=2; (${LINES_IN_FILES[$COUNTER3]} * ${SUMXY} - ${SUMX} * ${SUMY}) / (${LINES_IN_FILES[$COUNTER3]} * ${SUMX2} - ${SUMX} * ${SUMX})" | bc)
+    B=$(echo "scale=2; (${SUMY} - ${A} * ${SUMX}) / ${LINES_IN_FILES[$COUNTER3]}" | bc)
+    ERR1=$(echo "scale=2; ${Y_ARRAY[$((i + FIXER))]}/1" | bc)
+    ERR2=$(echo "scale=2; (${A} * ${X_ARRAY[$((i + FIXER))]} + ${B})/1" | bc )
+    ERR=$(echo "scale=2;  ${ERR} + (${ERR1} - ${ERR2}) * (${ERR1} - ${ERR2})" | bc )        #In manual pages of bc explained that subtraction won't read scale variable so we divide by 1
 
   done
   FIXER=$FIXER+${LINES_IN_FILES[$COUNTER3]}
